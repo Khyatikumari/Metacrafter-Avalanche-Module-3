69240d85b34d4930f47f38c9b16694ea8aa1ab7b
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-02-07 13:21:10 +0530

    Time: 172 ms (77.26%), Space: 20.1 MB (55.07%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        res = 0
+        count = 0
+        for ch in s:
+            if ch == 'b':
+                count += 1
+            elif count:
+                res += 1
+                count -= 1
+        
+        return res    
