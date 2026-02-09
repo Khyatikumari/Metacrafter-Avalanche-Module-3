7a564120830fc96f10b40751b5c7aa13d07a861a
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-02-09 11:51:42 +0530

    Time: 15 ms (51.89%), Space: 65.8 MB (42.05%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,25 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    vector<int> nums;
 public:
-    TreeNode* balanceBST(TreeNode* root) {
-        nums.clear();
-        getNumbers(root);
-        return balanceTree(0, nums.size() - 1);
+    void inorder(TreeNode* root, vector<int>& v){
+        if(!root) return;
+        inorder(root->left, v);
+        v.push_back(root->val);
+        inorder(root->right, v);
     }
-private:
-    void getNumbers(TreeNode* node) {
-        if (node == nullptr) return;
-        getNumbers(node->left);
-        nums.push_back(node->val);
-        getNumbers(node->right);
-    }
-    TreeNode* balanceTree(int l, int r) {
-        if (l > r) return nullptr;
-        int middleIdx = l + (r - l) / 2;
-        TreeNode* root = new TreeNode(nums[middleIdx]);
-        root->left = balanceTree(l, middleIdx - 1);
-        root->right = balanceTree(middleIdx + 1, r);
+    TreeNode* build(int l, int r, vector<int>&v){
+        if(l>r) return nullptr;
+        int mid= (l+r)/2;
+        TreeNode* root = new TreeNode(v[mid]);
+        root->left = build(l, mid-1, v);
+        root->right = build(mid+1, r, v);
         return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int>v;
+        inorder(root,v);
+        return build(0, v.size()-1, v);
     }
 };
