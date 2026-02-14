e0403cf7e94506ffb6926e378d5ab84cc8988a96
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-02-14 11:01:14 +0530

    Time: 1 ms (100%), Space: 46.5 MB (98.23%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -1,19 +1,16 @@
 class Solution {
-    public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] dp = new double[101][101];
-        dp[0][0] = poured;
-
-        for (int r = 0; r <= query_row; r++) {
-            for (int c = 0; c <= r; c++) {
-                if (dp[r][c] > 1) {
-                    double overflow = (dp[r][c] - 1) / 2.0;
-                    dp[r][c] = 1;
-                    dp[r + 1][c] += overflow;
-                    dp[r + 1][c + 1] += overflow;
-                }
+    public double champagneTower(int poured, int row, int col) {
+        double[] dp = new double[col + 2];
+        dp[0] = poured;
+        for(int i = 0; i < row; i++) {
+            for(int j = Math.min(i, col); j >= 0; j--) {
+                if(dp[j] > 1) {
+                    double val = (dp[j] - 1) / 2.0;
+                    dp[j] = val;
+                    dp[j + 1] += val;
+                }else dp[j] = 0;
             }
         }
-
-        return Math.min(1, dp[query_row][query_glass]);
+        return Math.min(1, dp[col]);
     }
 }
